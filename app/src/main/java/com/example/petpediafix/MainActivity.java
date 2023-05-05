3489a7597cf3b17d37d5d401bc64fd1aeb8d8364
AlexaGTorres <alexa.torres19122694@gmail.com>
2023-05-05 12:27:04 -0700

    Working splash screen, just need the main menu screen imported.

--- app/src/main/java/com/example/petpediafix/MainActivity.java
+++ app/src/main/java/com/example/petpediafix/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.petpediafix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+       // Intent home = new Intent(MainActivity.this, MainMenu.class);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                //startActivity(home);
+                finish();
+            }
+        }, 4000);
     }
 }
