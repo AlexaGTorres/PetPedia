62ee794c9d68a20cddaace3c7105b4bc1d688cd6
AlexaGTorres <alexa.torres122694@gmail.com>
2023-05-11 12:22:47 -0700

    Added list view and customized text.

--- app/src/main/java/com/example/petpediafix/ListOfAnimals.java
+++ app/src/main/java/com/example/petpediafix/ListOfAnimals.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ListOfAnimals extends AppCompatActivity {
+public class ListOfAnimals extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_animals);
     }
