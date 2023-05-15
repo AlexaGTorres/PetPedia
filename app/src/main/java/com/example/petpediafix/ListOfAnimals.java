72a8f209be5f4755e6d392572433277ea627a7c5
AlexaGTorres <alexa.torres19122694@gmail.com>
2023-05-15 11:58:00 -0700

    Sample list filled up.

--- app/src/main/java/com/example/petpediafix/ListOfAnimals.java
+++ app/src/main/java/com/example/petpediafix/ListOfAnimals.java
@@ -3,14 +3,35 @@ package com.example.petpediafix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListOfAnimals extends AppCompatActivity
 {
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_animals);
+
+        listView = (ListView) findViewById(R.id.animalsList);
+
+        ArrayList<String> animalList = new ArrayList<>();
+
+        animalList.add("Dog 1");
+        animalList.add("Dog 2");
+        animalList.add("animal");
+        animalList.add("animal");
+        animalList.add("animal");
+        animalList.add("animal");
+        animalList.add("animal");
+        animalList.add("animal");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, animalList);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
