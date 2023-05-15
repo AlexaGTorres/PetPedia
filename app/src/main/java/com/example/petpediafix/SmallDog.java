38cf78681c32cc6cbafff17bc0958159b9d81b2e
AlexaGTorres <alexa.torres19122694@gmail.com>
2023-05-15 12:04:52 -0700

    Created smallDog information screen.

---
+++ app/src/main/java/com/example/petpediafix/SmallDog.java
@@ -0,0 +1,14 @@
+package com.example.petpediafix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmallDog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_small_dog);
+    }
+}
